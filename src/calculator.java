126f332a8dcf30f46727bfd7e167a13441a5f84d
Abraham-Jerin-John <abrahamkalarickal98@gmail.com>
2022-11-21 10:21:15 +0530

    input reading

---
+++ src/calculator.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class calculator {
+    public static void main(String[] args) {
+        int a,b;
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter first number:");
+        a=sc.nextInt();
+        System.out.println("Enter second number:");
+        b=sc.nextInt();
+
+
+
+    }
+}
